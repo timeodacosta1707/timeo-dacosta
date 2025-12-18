dcf8ccfc59b056fb1769e45e570d7989ca64f04a
dacostatimeo <dacostatimeo@gmail.com>
2025-12-18 02:03:32 +0100

    update

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Important pour [class.open]
 
 @Component({
-  selector: 'app-navbar',
-  imports: [ RouterLink ],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+	selector: 'app-navbar',
+	standalone: true,
+	imports: [RouterLink, RouterLinkActive, CommonModule],
+	templateUrl: './navbar.html',
+	styleUrl: './navbar.scss',
 })
 export class Navbar {
+	isMenuOpen = false;
 
+	toggleMenu() {
+		this.isMenuOpen = !this.isMenuOpen;
+
+		if (this.isMenuOpen) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'auto';
+		}
+	}
+
+	closeMenu() {
+		this.isMenuOpen = false;
+		document.body.style.overflow = 'auto';
+	}
 }
